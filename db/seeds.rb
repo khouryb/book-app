55a3e978d36ae8c22c5e8df52fc889483a3ed3a0
Benjamin Khoury <khourybenjamin@gmail.com>
2023-05-24 08:39:57 +0100

    trying to seed, getting error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Author.create(name: 'George Orwell', dob: Date.new(1903, 06, 25), books_published: 9)
+Author.create(name: 'J. D. Salinger', dob: Date.new(1919, 01, 01), books_published: 15)
+Author.create(name: 'Stephen King', dob: Date.new(1947, 9, 21), books_published: 65)
+
+
